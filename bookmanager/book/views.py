33ba2a648308a73b87af16f8a91e984ecaf8e493
Huangfu Star <huangfustar@qq.com>
2022-01-02 18:24:28 +0800

    Define the view function im book

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
+
+# route address: /index/
+def index(request):
+    response = HttpResponse('ok')
+    return response
