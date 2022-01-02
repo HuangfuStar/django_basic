2f6cd980d7c12b9b4280181bf2e830117b295fcd
Huangfu Star <huangfustar@qq.com>
2022-01-02 17:38:55 +0800

    Add two models in bookmanager/book/models.py

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+"""
+1. 我们的模型类 需要继承自 models.Model
+2. 没有指定主键时, 迁移创建数据表的时候会自动为我们添加一个主键 -- id
+3. 字段
+    字段名 = models.类型(选项)
+    字段名
+
+"""
+
+
+class BookInfo(models.Model):
+    # 会自动创建一个 id 字段
+    name = models.CharField(max_length=50)
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=50)
+    gender = models.BooleanField()
+    # 设置外键约束外键约束
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
